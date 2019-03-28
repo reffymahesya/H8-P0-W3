ca4a9a75740f367609a197d1ecc58f6b2164f4cd
reffymahesya <reffymahesyar@gmail.com>
2019-03-28 10:13:41 +0700

    handle Z

--- Exercise 05 - Ubah Huruf/Exercise 5.js
+++ Exercise 05 - Ubah Huruf/Exercise 5.js
@@ -1,6 +1,6 @@
 function ubahHuruf(kata) {
   // you can only write your code here!
-  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q', 'r','s','t','u','v','w','x','y','z']
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q', 'r','s','t','u','v','w','x','y','z','a']
   var result = ''
   var a = kata.length
 
